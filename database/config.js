5c08531d8c71851f69b3b8618f935ed2a1d797e6
William Morataya Molina <wmorataya@tribalworldwide.gt>
2022-07-22 12:32:39 -0600

    Levantar Mongo

--- database/config.js
+++ database/config.js
@@ -1,29 +1,20 @@
 const mongoose = require('mongoose');
 
 const dbConnection = async() => {
-    // try {
-        // await mongoose.connect(process.env.MONGODB_CNN, {
-        //     useNewUrlParser: true,
-        //     useUnifiedTopology: true,
-        //     useCreateIndex: true,
-        //     useFindAndModify: false
-        //   });
-
-        // console.log('Base de datos online');
-
-        await mongoose.connect(process.env.MONGODB_CNN, 
-            {
-            useNewUrlParser: true, 
+    try {
+        await mongoose.connect(process.env.MONGODB_CNN, {
+            useNewUrlParser: true,
             useUnifiedTopology: true,
-            }, err => {
-            if(err) throw err;
-            console.log('Connected to MongoDB!!!')
-            });
+            useCreateIndex: true,
+            useFindAndModify: false
+          });
 
-    // } catch (error) {
-    //     console.log(error);
-    //     throw new Error('Error a la hora de iniciar la base de datos');
-    // }
+        console.log('Base de datos online');
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de iniciar la base de datos');
+    }
 }
 
 module.exports = {
